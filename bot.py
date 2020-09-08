7a235add229eb04215f8a0d1e2b884ba81e013e1
yash-clear <yash-clear@github.com>
2020-09-08 23:21:55 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,65 @@
+
+import discord
+import requests
+import joke_api
+import os
+
+from dotenv import load_dotenv
+URL = 'https://official-joke-api.appspot.com/random_joke'
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+from discord.ext import commands
+#GUILD='CodingClub-Development'
+client = commands.Bot(command_prefix="!")
+
+
+@client.command(pass_context=True)
+async def DM(ctx, user: discord.User, *, message=None):
+    message = message or "This Message is sent via DM"
+    await user.send_message(user, message)
+
+
+@client.command(help="it will print whatever u write")
+async def print(ctx, *args):
+	response = ""
+
+	for arg in args:
+		response = response + " " + arg
+
+	await ctx.channel.send(response)
+
+
+
+@client.command(help="ensuring the working of client",brief="Prints pong back to the channel.")
+async def ping(ctx):
+	await ctx.channel.send("pong")
+	
+'''
+@client.event
+async def on_message(message):
+	if message.content.startswith == "!hello":
+		await message.channel.send("Hello, I hope you brought a pizza for me")
+
+	await client.process_commands(message)
+ '''
+
+
+@client.command(help="I have lots of jokes")
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!joke'):
+        joke = joke_api.get_joke()
+        print(joke)
+
+        if joke == False:
+            await message.channel.send("Couldn't get joke from API. Try again later.")
+        else:
+            await message.channel.send(joke['setup'] + '\n' + joke['punchline'] )
+        await client.process_commands(message)
+
+
+client.run(DISCORD_TOKEN)
